6a4b5e9a23254584040bd90e07dd188e300b0a29
JenyaIII <161russ@gmail.com>
2020-03-06 17:23:23 +0300

    Update linter config, add interfaces, update TodoList, TodoFrom, Fixed preventDefault bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
 import TodoForm from './components/TodoFrom';
+import TodoList from './components/TodoList';
+import { ITodo } from './interfaces';
 
 const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    // setTodos([newTodo, ...todos])
+    setTodos(prev => [newTodo, ...prev])
+  }
+
+  const toggleHandler = (id: number) => {
+    setTodos(prev => prev.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  const removeHandler = (id: number) => {
+    const shoudRemove = window.confirm('Do you want delete this todo ?');
+    if (shoudRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+  }
+  
   return (
   <>
     <Navbar />
     <div className="container">
-      <TodoForm />
+      <TodoForm onAdd={addHandler} />
+      <TodoList
+      todos={todos}
+      onToggle={toggleHandler}
+      onRemove={removeHandler}
+      />
     </div>
   </>
   );
